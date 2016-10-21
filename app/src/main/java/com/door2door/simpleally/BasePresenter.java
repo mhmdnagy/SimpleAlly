689b42de1a55b3d194f0137611b38b2235d29510
vezikon <vezikon@hotmail.com>
2016-10-21 23:22:11 +0200

    Adding base mvp interfaces

---
+++ app/src/main/java/com/door2door/simpleally/BasePresenter.java
@@ -0,0 +1,12 @@
+package com.door2door.simpleally;
+
+/**
+ * Created by vezikon on 10/19/16.
+ */
+
+public interface BasePresenter {
+
+    void bind();
+
+    void unbind();
+}
---
+++ app/src/main/java/com/door2door/simpleally/BaseView.java
@@ -0,0 +1,10 @@
+package com.door2door.simpleally;
+
+/**
+ * Created by vezikon on 10/19/16.
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
